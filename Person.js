c0c44ba95c61310ef2970aef03e46f30f01a8de0
Nkemakolam <nwokoronicholas@yahoo.com>
2024-09-26 20:31:53 +0200

    Class Dish

--- Person.js
+++ Person.js
@@ -4,4 +4,14 @@ class Dish {
         this.price = price;
         this.ingredients = ingredients;
     }
+
+    // To calculate the total cost of a dish using the fixed cost to be 10
+    cost = () => {
+        const costOfIngredients = this.ingredients.reduce((total, ingredient) => total + ingredient.cost, 0);
+        return 10 + costOfIngredients;
+    }
+
+    profit = () => {
+        return this.price - this.cost();
+    }
 }
