f772eacf7d25a0ae0b40a826cb205e8d093fcf0f
Nkemakolam <nwokoronicholas@yahoo.com>
2024-09-28 12:01:18 +0200

    New changes

--- Person.js
+++ Person.js
@@ -1,3 +1,14 @@
+// Side project to log to the browser instead of the console
+const displayInBrowser = (content) => {
+    let container = document.getElementById("console")
+    content.forEach(element => {
+        let newDiv = document.createElement("div")
+        newDiv.innerText = element
+        container.appendChild(newDiv)
+    });
+}
+
+
 class Dish {
     constructor(food, price, ingredients) {
         this.food = food;
@@ -29,4 +40,31 @@ class Client {
         this.id = id;
     }
 }
+
+
+class Restaurant {
+    constructor(restaurantName) {
+        this.restaurantName = restaurantName;
+        this.orders = [];
+    }
+
+    orderDish =(dish, client)=> {
+        this.orders.push({dish, client});
+    }
+}
+
+// Ingredients
+const cheese = new Ingredient('Cheese', 5);
+const pepperoni = new Ingredient('Pepperoni', 7);
+const dough = new Ingredient('Dough', 3);
+const lettuce = new Ingredient('Lettuce', 2);
+const tomato = new Ingredient('Tomato', 4);
+
+// Dishes
+const pizza = new Dish('Pizza', 30, [cheese, pepperoni, dough]);
+const salad = new Dish('Salad', 25, [lettuce, cheese, tomato]);
+
+// Clients
+const pluto = new Client('Pluto', 1);
+const goofy = new Client('Goofy', 2);
 
